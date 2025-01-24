433e7b51d806d5910994e4738662c8a933f937c6
Bradobrey27 <serega10011978serega@gmail.com>
2025-01-24 15:26:57 +0200

    little changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { provideRouter, Routes, withHashLocation } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: '', redirectTo: 'main/home', pathMatch: 'full' },
   {
     path: 'main',
     loadComponent: () =>
